2ef5e11bf05b0f0830cf082cb9a11767563f8390
Ian Hamilton <ian.hamilton117@gmail.com>
2018-07-26 11:23:09 -0700

    Adding random strings to use as search terms

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -11,14 +11,14 @@ export class RecipeService {
   private recipes: Recipe[] = [
     new Recipe(
       'Tasty Schnitzel',
-      'A super-tasty Schnitzel - just awesome!',
+      'A super-tasty Schnitzel - just awesome! viseheiubseusevnbeiwooyfneoqpshg',
       'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG',
       [
         new Ingredient('Meat', 1),
         new Ingredient('French Fries', 20)
       ]),
     new Recipe('Big Fat Burger',
-      'What else you need to say?',
+      'What else you need to say? iauwdlaihfsdufisenwijpoiuwWienuvy',
       'https://upload.wikimedia.org/wikipedia/commons/b/be/Burger_King_Angus_Bacon_%26_Cheese_Steak_Burger.jpg',
       [
         new Ingredient('Buns', 2),
